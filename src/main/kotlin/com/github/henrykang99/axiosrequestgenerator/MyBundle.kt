601d30355b364da2842be579364a4eb803c7b61d
GitHub Action <action@github.com>
2024-11-17 08:30:56 +0000

    Template cleanup

--- src/main/kotlin/com/github/henrykang99/axiosrequestgenerator/MyBundle.kt
+++ src/main/kotlin/com/github/henrykang99/axiosrequestgenerator/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.henrykang99.axiosrequestgenerator
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/henrykang99/axiosrequestgenerator/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/henrykang99/axiosrequestgenerator/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.henrykang99.axiosrequestgenerator.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger
