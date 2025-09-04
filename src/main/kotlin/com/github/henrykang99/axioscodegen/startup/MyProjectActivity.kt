8ee28b9a9d219642af1268ac5cdbccc3f4c9eff8
GitHub Action <action@github.com>
2025-09-04 07:23:07 +0000

    Template cleanup

--- src/main/kotlin/com/github/henrykang99/axioscodegen/startup/MyProjectActivity.kt
+++ src/main/kotlin/com/github/henrykang99/axioscodegen/startup/MyProjectActivity.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.startup
+package com.github.henrykang99.axioscodegen.startup
 
 import com.intellij.openapi.diagnostic.thisLogger
 import com.intellij.openapi.project.Project
